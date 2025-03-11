0006ee9f22dde8bafb65524b491ee46eeb2d5f68
HasnainAliPK <hasnain2640@hotmail,com>
2025-03-11 12:06:14 +0100

    Added properties and methods in the Class Car

---
+++ BridgeLibrary/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLibrary
+{
+    public class Car
+    {
+        public string LicensePlate { get; private set; }
+        public DateTime Date { get; private set; }
+
+        public double Price()
+        {
+            return 230;
+        }
+
+        public string VehicleType()
+        {
+            return "Car";
+        }
+    }
+}
