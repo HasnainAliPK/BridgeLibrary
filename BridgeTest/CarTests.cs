67a036167aebcaab9d421b42cc3a906d63d095e3
HasnainAliPK <hasnain2640@hotmail,com>
2025-03-11 12:39:26 +0100

    Tested class Car

---
+++ BridgeTest/CarTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bridge;
+using BridgeLibrary;
+
+namespace Bridge.Test
+{
+    [TestClass]
+    public class CarTests
+    {
+
+        [TestMethod]
+        public void Price_ShouldReturn230() 
+        {
+            var car = new Car();
+            Assert.AreEqual(230, car.Price());
+        }
+
+        [TestMethod]
+        public void VehicleType_ShouldReturnCar()
+        { 
+            var car = new Car();
+            Assert.AreEqual("Car", car.VehicleType());
+        }
+    }
+}
